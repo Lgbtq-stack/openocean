0859281671a1ab8c81c7bff90051b3177f7de5fb
Vlad <lgbtqtoken366@gmail.com>
2025-01-06 20:19:46 +0100

    +

--- index.js
+++ index.js
@@ -31,3 +31,36 @@ function showNextSlide() {
 }
 
 setInterval(showNextSlide, 5000);
+
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTopButton.style.display = 'flex';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
+const popup = document.getElementById("popup-module");
+
+function showPopup(message, canClose = true) {
+    if (popup) {
+        popup.querySelector(".popup-content").textContent = message;
+        popup.style.display = "flex";
+        closePopupButton.style.display = canClose ? "block" : "none";
+
+        if (canClose) {
+            closePopupButton.addEventListener("click", () => {
+                popup.style.display = "none";
+            });
+        }
+    }
+}
