62c2fb8a4ae9d0b00ecf076447fe907768dfe81c
Lgbtq-stack <lgbtqtoken366@gmail.com>
2025-04-03 12:12:08 +0300

    +

--- js/ProductDetailsLogic.js
+++ js/ProductDetailsLogic.js
@@ -228,7 +228,7 @@ export async function showLimitedNFTDetails(id, dataSource) {
     document.getElementById('limited-purchase-button').onclick = () => {
         const popup = document.getElementById('are-you-sure-popup');
         const message = document.getElementById('are-you-sure-message');
-        message.innerHTML = `Are you sure you want to purchase \br <strong>${quantity} item(s)</strong> of <strong>${nft.name}</strong>?`;
+        message.innerHTML = `Are you sure you want to purchase \br <strong>${quantity} NFT(s)</strong> of <strong>${nft.name}</strong>?`;
         popup.classList.remove('hidden');
 
         document.querySelector('.are-you-sure-popup-close').onclick = () => popup.classList.add('hidden');
